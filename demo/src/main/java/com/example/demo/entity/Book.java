23ac261f6bab9bcfdf84991b2bd699874dfd5be9
Nguyen Loan <Admin@LAPTOP-Q62KFUJF>
2023-05-30 22:26:10 +0700

    nop bai tuan 2

--- demo/src/main/java/com/example/demo/entity/Book.java
+++ demo/src/main/java/com/example/demo/entity/Book.java
@@ -1,6 +1,12 @@
 package com.example.demo.entity;
+import com.example.demo.validator.annotation.ValidCategoryId;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
+import jakarta.validation.Constraint.*;
+
 @Data
 @Entity
 @Table(name="book")
@@ -9,13 +15,20 @@ public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(name = "title")
+    @NotEmpty(message = "Title must to be empty")
+    @Size(max = 50, min = 1, message = "Title must be less than 50 characters")
     private String title;
+
     @Column(name = "author")
     private String author;
+
     @Column(name = "price")
+    @NotNull(message = "Price is required")
     private Double price;
     @ManyToOne
     @JoinColumn(name = "category_id")
+    @ValidCategoryId
     private Category category;
 }
